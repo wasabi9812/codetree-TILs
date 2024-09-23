1a8769a9b6085b7394e9a26bb7d13e5782e334bc
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-09-23 17:06:42 +0900

    Title: 사칙연산을 이행하는 함수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240923/사칙연산을 이행하는 함수/functions-that-carry-out-four-factor-operations.py
@@ -0,0 +1,37 @@
+import sys
+
+# 입력을 받아서 분리
+x, a, y = sys.stdin.readline().split()
+x = int(x)
+y = int(y)
+
+# 함수 정의를 위로 이동
+def su(x, y):
+    r = x + y
+    return r
+
+def minus(x, y):
+    r = x - y
+    return r
+
+def multi(x, y):
+    r = x * y
+    return r
+
+def div(x, y):
+    r = x / y
+    return r
+
+# 연산 수행
+if a == "+":
+    result = su(x, y)
+    print(f"{x} {a} {y} = {result}")
+elif a == "-":
+    result = minus(x, y)
+    print(f"{x} {a} {y} = {result}")
+elif a == "*":
+    result = multi(x, y)
+    print(f"{x} {a} {y} = {result}")
+else:
+    result = div(x, y)
+    print(f"{x} {a} {y} = {result}")
