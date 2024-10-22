0939eade98ee8b331dce491a524b2cbfd5b4d704
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-10-22 17:56:06 +0900

    Title: 숫자 차례로 출력하기, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241022/숫자 차례로 출력하기/to-output-numerically.py
+++ 241022/숫자 차례로 출력하기/to-output-numerically.py
@@ -4,17 +4,16 @@ N = int(sys.stdin.readline())
 
 def recur1(n):
     if(n == 0):
-        print(" ")
         return
     print(n, end=' ')
-    recur(n-1)
+    recur1(n-1)
 
 def recur2(n):
     if(n == 0):
-        print(" ")
         return
-    recur(n-1)
+    recur2(n-1)
     print(n, end=' ')
+
 
 recur1(N)
 print()
