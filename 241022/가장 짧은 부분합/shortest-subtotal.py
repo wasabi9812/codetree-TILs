c7380af683fd6e23934c96dc8be096c0419d602e
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-10-22 22:35:41 +0900

    Title: 가장 짧은 부분합, Time: 141ms, Memory: 31MB, Status: Passed - CodeTree

--- 241022/가장 짧은 부분합/shortest-subtotal.py
+++ 241022/가장 짧은 부분합/shortest-subtotal.py
@@ -8,18 +8,15 @@ minimal_length = float('inf')
 sum_val = 0
 j = 0
 for i in range(1, n+1):
-    while j+1 <= n and sum_val <s:
-        sum_val += arr[j+1]
+    while j + 1 <= n and sum_val < s:
         j += 1
+        sum_val += arr[j]
 
-        
+    # sum_val이 s 이상이면 minimal_length 갱신
+    if sum_val >= s:
+        minimal_length = min(minimal_length, j - i + 1)
 
-        if sum_val >= s:
-            minimal_length = min(minimal_length, j-i+1)
-        
-    if sum_val < s:
-        break
-
+    # 다음 구간으로 넘어가기 전에 현재 구간의 시작점 i를 제외
     sum_val -= arr[i]
 
 if minimal_length == float('inf'):
