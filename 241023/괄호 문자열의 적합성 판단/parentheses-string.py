db1ba4081c5ab5b9171a45032d7660a5229c2819
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-10-23 22:22:03 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 241023/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 241023/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -1,18 +1,20 @@
 import sys
 
 s = []
+data = list(map(str, sys.stdin.readline().strip()))  # 입력을 문자 하나하나로 받음
 
-cur = 0
+for char in data:
+    if char == "(":
+        s.append("(")  # 여는 괄호를 스택에 추가
+    elif char == ")":
+        if s:  # 스택에 여는 괄호가 있으면 pop
+            s.pop()
+        else:  # 스택이 비어 있는데 닫는 괄호가 나왔다면 잘못된 괄호
+            print("No")
+            exit()
 
-data = list(map(str,sys.stdin.readline().split()))
-
-for i in range(len(data)):
-    if data[i] == "(":
-        cur += 1
-    else:
-        cur -= 1
-
-if cur ==0:
+# 모든 괄호가 짝이 맞는지 확인
+if not s:
     print("Yes")
 else:
     print("No")
