0989e296f86bfd5879226cd673f330e04230b641
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-10-24 21:02:46 +0900

    Title: 수열 재조합, Time: 204ms, Memory: 33MB, Status: Passed - CodeTree

--- 241024/수열 재조합/recombination-of-sequences.py
+++ 241024/수열 재조합/recombination-of-sequences.py
@@ -3,15 +3,32 @@ import sys
 N = int(sys.stdin.readline())
 stack = []
 last = 0
-for i in range(1,N+1):
+output = []
+possible = True
+
+for i in range(N):
     m = int(sys.stdin.readline())
-    if last<m:
-        for j in range(last + 1,m+1):
+    
+    # 스택에 더 큰 값 추가
+    if last < m:
+        for j in range(last + 1, m + 1):
             stack.append(j)
-            print("+")
+            output.append("+")
         last = m
+    
+    # 스택 최상단이 m과 같다면 pop
     if stack:
         top_value = stack[-1]
         if top_value == m:
             stack.pop()
-            print("-")
+            output.append("-")
+        else:
+            # 스택의 최상단 값이 현재 필요한 값과 다르다면 수열을 만들 수 없음
+            possible = False
+            break
+
+if possible:
+    for op in output:
+        print(op)
+else:
+    print("NO")
