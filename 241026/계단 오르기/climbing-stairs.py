59d9accf4423b3ca185e6eeff8e2f6ed1dc120ba
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-10-26 16:24:15 +0900

    Title: 계단 오르기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 241026/계단 오르기/climbing-stairs.py
+++ 241026/계단 오르기/climbing-stairs.py
@@ -9,7 +9,7 @@ dp[2], dp[3] = 1, 1
 def climb(n):
     # 4번째 계단부터 n번째 계단까지 dp 배열 채우기
     for i in range(4, n + 1):
-        dp[i] = dp[i - 2] + dp[i - 3]
+        dp[i] = (dp[i - 2] + dp[i - 3])%10007
     return dp[n]
 
 print(climb(N))
