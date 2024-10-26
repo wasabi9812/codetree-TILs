55cee3910c58c3e7495ffaa898bb59e7504ce669
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-10-26 16:06:59 +0900

    Title: 피보나치 수, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 241026/피보나치 수/fibonacci-number.py
+++ 241026/피보나치 수/fibonacci-number.py
@@ -1,11 +1,12 @@
 import sys
 
 N = int(sys.stdin.readline())
-def fibo(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fibo(n-1)+fibo(n-2)
-
-print(fibo(N))
+def bottom_up(n):
+    if n<1:
+        return n
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n+1):
+        dp[i] = dp[i-1]+dp[i-2]
+    print(dp[n])
+bottom_up(N)
