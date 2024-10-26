4d22f140d75811c52ffb9ee35bf6cb59c96f1fa3
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-10-26 16:22:57 +0900

    Title: 계단 오르기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241026/계단 오르기/climbing-stairs.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+# 필요한 초기값만 설정
+dp = [0] * (N + 5)
+dp[2], dp[3] = 1, 1
+
+def climb(n):
+    # 4번째 계단부터 n번째 계단까지 dp 배열 채우기
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 3]
+    return dp[n]
+
+print(climb(N))
