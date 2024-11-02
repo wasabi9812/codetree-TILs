6f130f0947560de0e27b4ae13fc7813e97cb7083
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-11-02 17:05:41 +0900

    Title: 문자열 정렬, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

--- 241102/문자열 정렬/string-sort.py
+++ 241102/문자열 정렬/string-sort.py
@@ -1,11 +1,11 @@
 import sys
 
-data = list(map(str ,sys.stdin.readline().split()))
+data = list(map(str ,sys.stdin.readline().strip()))
 
 for i in range(len(data)-1):
     for j in range(len(data)-1,i,-1):
-        if data[j-1]<data[j]:
+        if data[j-1]>data[j]:
             data[j-1],data[j] = data[j], data[j-1]
 
-for i in range(len(data)-1):
+for i in range(len(data)):
     print(data[i],end ='')
