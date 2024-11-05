59705c696b23db73d8ad2fa8c8a20b0b42ca3869
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-11-05 12:31:50 +0900

    Title: 2개씩 그룹짓기, Time: 43ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241105/2개씩 그룹짓기/group-of-pairs.py
@@ -0,0 +1,18 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+data = list(map(int,sys.stdin.readline().split()))
+
+def sortnum(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1, i , -1):
+            if arr[j-1]>arr[j]:
+                arr[j-1], arr[j] = arr[j], arr[j-1]
+
+sortnum(data)
+sum_val = 0
+for i in range(N):
+    sum_val+=data[N-i]
+
+print(sum_val)
