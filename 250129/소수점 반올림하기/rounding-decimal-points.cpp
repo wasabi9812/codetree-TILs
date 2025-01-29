b1d0eeefb81939701a3b639295493dc51fbc2751
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2025-01-29 16:48:22 +0900

    Title: 소수점 반올림하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250129/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    double a =25.352;
+    cout.precision(1);
+    cout << a;
+    return 0;
+}
