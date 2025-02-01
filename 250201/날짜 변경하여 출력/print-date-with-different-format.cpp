bbdc5721b89913b8a107be541ee2044aff3b18cd
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2025-02-01 18:30:35 +0900

    Title: 날짜 변경하여 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250201/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    char d;
+    char e = "-";
+    cin>>a>>d>>b>>d>>c;
+    cout <<a<<e<<b<<e<<c;
+    return 0;
+}
