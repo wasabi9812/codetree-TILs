6a776ba27629c0a746dc6d0bf5ee45193f2cb8d3
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2025-02-01 18:08:28 +0900

    Title: 입력받은 값 교체하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250201/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    int temp = a;
+    a=b;
+    b =temp;
+    cout <<a<" "<<b;
+    return 0;
+}
