e1fc83ce596624708d54f06c0424aaa0757c417b
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2025-02-01 18:33:42 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250201/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a,b,c;
+    char d;
+    cin >>a>>d>>b>>d>>c;
+    cout <<a<<"-"<<c<<"-"<<b;
+    return 0;
+}
