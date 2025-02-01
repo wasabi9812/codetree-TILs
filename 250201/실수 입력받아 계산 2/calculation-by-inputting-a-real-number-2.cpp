eb3cfd4efbba7655c643f56e48e6790b100816ee
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2025-02-01 18:05:48 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250201/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout <<fixed;
+    cout.precision(2);
+    double a;
+    cin>>a;
+    cout<<a+1.5;
+    return 0;
+}
