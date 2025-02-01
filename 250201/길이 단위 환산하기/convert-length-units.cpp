f461546dfa49038e191d655a05dbe0d94a0dee0a
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2025-02-01 18:03:30 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250201/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout <<fixed;
+    cout.precision(1);
+    int a;
+    cin>>a;
+    cout<<a*30.48;
+    return 0;
+}
