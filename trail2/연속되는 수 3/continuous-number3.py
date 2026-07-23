b5248e165e021782ab8cb13152172a43c8596c6d
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2026-07-23 14:17:41 +0900

    [Passed] 연속되는 수 3 (64ms, 16MB)

---
+++ trail2/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+nlist = []
+for i in range(n):
+    a = int(input())
+    nlist.append(a)
+
+result = 0
+cnt = 0
+for i in range(n):
+    if i == 0:
+        cnt = 1
+    elif i>=1 and nlist[i]*nlist[i-1] > 0:
+        cnt+=1
+    else:
+        cnt = 1
+    result = max(result, cnt)
+
+print(result)
