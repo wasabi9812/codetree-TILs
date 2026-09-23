36d3afbb720a661d5e6fcf9c8ca50df12c5a9f8f
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2026-09-23 17:16:40 +0900

    [Passed] 변수 값 복사하기 2 (2ms, 0MB)

---
+++ trail1/변수 값 복사하기 2/copying-variable-values-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    a = b = c;
+    cout<<a<<" "<< b << " "<< c;
+
+
+    return 0;
+}
